e5e9f9df277d5f50256f6c1cba58aaef7faff71d
2-Davinci <116849278+2-Davinci@users.noreply.github.com>
2023-03-31 17:03:19 +0000

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/ stylestyles/globals.css'
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
